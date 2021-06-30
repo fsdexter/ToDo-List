c6e5a1cc2e1c70018a8ddde9e6886861712fabfc
fsdexter <fsdexter@gmail.com>
2021-06-30 17:05:45 +0200

    done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -13,14 +13,22 @@ export function Home() {
 	const [newTask, setNewTask] = useState("");
 
 	function del(i) {
-		setToDo(toDo.filter((tarea, index) => index !== i));
+		setToDo(toDo.filter((iten, index) => index !== i));
 	}
 
 	const toDoList = toDo.map((task, i) => {
 		return (
 			<li key={i}>
-				<button onClick={() => del(i)} />
-				{task}
+				<div className="row">
+					<div className="col d-flex justify-content-between">
+						{task}
+						<i
+							id="trash"
+							className="fas fa-trash-alt"
+							onClick={() => del(i)}
+						/>
+					</div>
+				</div>
 			</li>
 		);
 	});
@@ -28,21 +36,37 @@ export function Home() {
 	// JS somente acima
 
 	return (
-		<>
-			<input
-				type="text"
-				placeholder="New Task"
-				onChange={event => {
-					setNewTask(event.target.value);
-				}}
-				onKeyPress={event => {
-					if (event.key === "Enter") {
-						setToDo([...toDo, event.target.value]);
-					}
-				}}
-			/>
-
-			<ul>{toDoList}</ul>
-		</>
+		<div className="container d-flex justify-content-md-center row mt-5">
+			<div id="caixa" className="col-7 ">
+				<h1 className="text-center">ToDo List</h1>
+				<div className="row justify-content-md-center">
+					<div className="col col-lg-8">
+						<center>
+							<input
+								value={newTask}
+								id="input"
+								type="text"
+								placeholder="New Task"
+								onChange={event => {
+									setNewTask(event.target.value);
+								}}
+								onKeyPress={event => {
+									if (event.key === "Enter") {
+										setToDo([...toDo, event.target.value]);
+										setNewTask("");
+									}
+								}}
+							/>
+						</center>
+					</div>
+				</div>
+				<div className="row justify-content-md-center">
+					<div className="col col-lg-8 mt-3">
+						<ul>{toDoList}</ul>
+					</div>
+				</div>
+				<div className="text-center">Made by Felipe</div>
+			</div>
+		</div>
 	);
 }
